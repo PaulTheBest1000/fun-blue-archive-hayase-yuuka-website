ef61d1ca6768b65a7ea0ddc9e8b2a74138fe03df
PaulTheBest1000 YT <paulandsam1000@gmail.com>
2024-12-14 16:33:26 +0800

    Update website

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
 function showMessage() {
-    alert('I LOVE HAYASE YUUKA!!!');
+    alert('I LOVE HAYASE YUUKA!!!'); // Show alert with the message
+
+    // Display the GIF
+    const gifContainer = document.getElementById('gif-container');
+    gifContainer.style.display = 'block';  // Show the GIF container
+
+    // Hide the GIF after 5 seconds (5000 ms)
+    setTimeout(() => {
+        gifContainer.style.display = 'none';  // Hide the GIF container
+    }, 5000); // 5000ms = 5 seconds
 }
